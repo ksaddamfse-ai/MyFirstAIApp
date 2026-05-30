1d86f39a6a69291480532b44ea45723fd0c9dd9c
Saddam Hussain <saddamk0410@gmail.com>
2026-05-30 15:00:12 +0530

    fix: suppress HTTPS redirect warning in development

--- Program.cs
+++ Program.cs
@@ -58,8 +58,10 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
-app.UseHttpsRedirection();
+else
+{
+    app.UseHttpsRedirection();
+}
 app.UseAuthorization();
 app.MapControllers();
 
